8c23d33d885930e6a4b67c24d48c451ec38c7f7d
vickylcodes <vickylee10@hotmail.com>
2023-07-13 18:26:13 +1000

    Stuck

---
+++ script.js
@@ -0,0 +1,7 @@
+const hamburger = document.getElementById("hamburger");
+const navMenu = document.getElementById("nav-menu");
+
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
+  navMenu.classList.toggle("change");
+});
