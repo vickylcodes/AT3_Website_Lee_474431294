e5f40cebe11b19d6632d217ab4fd7d9c0e278988
vickylcodes <vickylee10@hotmail.com>
2023-07-14 13:05:08 +1000

    Added gifs to homepage

--- script.js
+++ script.js
@@ -3,5 +3,12 @@ const navMenu = document.getElementById("nav-menu");
 
 hamburger.addEventListener("click", () => {
   hamburger.classList.toggle("active");
-  navMenu.classList.toggle("change");
+  navMenu.classList.toggle("active");
 });
+
+document.querySelectorAll(".nav-link").forEach((n) =>
+  n.addEventListener("click", () => {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+  })
+);
