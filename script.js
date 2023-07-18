f0811782417277390940763689e92741199c2918
vickylcodes <vickylee10@hotmail.com>
2023-07-18 17:41:07 +1000

    Filled out contact page

--- script.js
+++ script.js
@@ -14,3 +14,49 @@ $(document).ready(function () {
     })
   );
 });
+
+document
+  .getElementById("contact-form")
+  .addEventListener("submit", function (e) {
+    e.preventDefault(); // Prevent form submission
+
+    // Retrieve form data
+    var name = document.getElementById("name").value;
+    var email = document.getElementById("email").value;
+    var message = document.getElementById("message").value;
+
+    // Perform additional validation if needed
+
+    // Send form data to the server using AJAX or fetch API
+    // You can customize this part based on your server-side implementation
+
+    // Example using fetch API
+    fetch("process-form.php", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        name: name,
+        email: email,
+        message: message,
+      }),
+    })
+      .then(function (response) {
+        // Handle response from the server
+        // You can display a success message or handle errors here
+        console.log(response);
+      })
+      .catch(function (error) {
+        // Handle any error that occurred during the request
+        console.error(error);
+      });
+  });
+
+document.getElementById("help-link").addEventListener("mouseover", function () {
+  document.getElementById("help-popup").style.display = "block";
+});
+
+document.getElementById("help-link").addEventListener("mouseout", function () {
+  document.getElementById("help-popup").style.display = "none";
+});
