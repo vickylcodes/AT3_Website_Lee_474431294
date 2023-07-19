d8adce5ba7c3467abce0ed4c99f89fd37fab39a8
vickylcodes <vickylee10@hotmail.com>
2023-07-19 18:29:07 +1000

    Updated mobile for about.html

--- script.js
+++ script.js
@@ -1,20 +1,3 @@
-$(document).ready(function () {
-  const hamburger = document.getElementById("hamburger");
-  const navMenu = document.getElementById("nav-menu");
-
-  hamburger.addEventListener("click", () => {
-    hamburger.classList.toggle("active");
-    navMenu.classList.toggle("active");
-  });
-
-  document.querySelectorAll("#nav-menu .nav-link").forEach((n) =>
-    n.addEventListener("click", () => {
-      hamburger.classList.remove("active");
-      navMenu.classList.remove("active");
-    })
-  );
-});
-
 document
   .getElementById("contact-form")
   .addEventListener("submit", function (e) {
